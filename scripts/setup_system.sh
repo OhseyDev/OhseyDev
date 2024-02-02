84fb3489ce5f3e3b11262775b271466f08ed5a1c
Ohsey <admin@ohsey.me>
2024-02-02 14:31:22 +0000

    Update cfg/ and scripts/

--- scripts/setup_system.sh
+++ scripts/setup_system.sh
@@ -56,4 +56,73 @@ paru m68k-elf-gcc
 paru m68k-elf-gdb
 paru m68k-elf-newlib
 paru m68k-elf-toolchain
+#loongarch64-linux-gnu
+paru loongarch64-linux-gnu-binutils
+paru loongarch64-linux-gnu-linux-api-headers
+paru loongarch64-linux-gnu-glibc
+paru loongarch64-linux-gnu-gcc
+#xtensa-esp32-elf
+paru xtensa-esp32-elf-binutils
+paru xtensa-esp32-elf-newlib
+paru xtensa-esp32-elf-gcc
+#mips-elf
+paru mips-elf-binutils
+paru mips-elf-gcc
+#sh-elf
+paru sh-elf-binutils
+paru sh-elf-gcc
+paru sh-elf-newlib
+paru sh-elf-binutils-casio
+paru sh-elf-gcc-casio
+#rwth-psp
+paru rwth-psp-binutils
+paru rwth-psp-gcc
+paru rwth-psp-libc
+#mips64-linux-gnu
+paru mips64-linux-gnu-binutils
+paru mips64-linux-gnu-linux-api-headers
+paru mips64-linux-gnu-gcc
+paru mips64-linux-gnu-glibc
+#mips64-elf
+paru mips64-elf-binutils
+paru mips64-elf-gcc
+paru mips64-elf-newlib
+#i686-elf
+paru i686-elf-binutils
+paru i686-elf-gcc
+paru i686-elf-gdb
+#i386-elf
+paru i386-elf-binutils
+paru i386-elf-gcc
+paru i386-elf-gdb
+#mips64-ultra-elf
+paru mips64-ultra-elf-binutils
+paru mips64-ultra-elf-gcc-stage1
+paru mips64-ultra-elf-newlib
+paru mips64-ultra-elf-gcc
+#arm-frc-linux-gnueabi
+paru arm-frc-linux-gnueabi-binutils
+paru arm-frc-linux-gnueabi-eglibc
+paru arm-frc-linux-gnueabi-gcc
+paru arm-frc-linux-gnueabi-wpilib
+#aarch64-ppp-elf
+paru aarch64-ppp-elf
+#osxcross
+paru osxcross-git
+#ps3toolchain
+paru ps3-env
+paru ps3-ppu-binutils
+paru ps3-ppu-gcc
+paru ps3-spu-binutils
+paru ps3-spu-gcc
+paru ps3-psl1ght
+paru ps3toolchain
+paru ps3-zlib
+paru ps3-zstd
+paru ps3-ffmpeg-libs
+paru ps3libraries
+
+# Install OhMyPosh
+
+curl -s https://ohmyposh.dev/install.sh | bash -s
 
--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# update arch repository packages
 pacman -Suy
+# update all installed aur packages
 paru -Sua
+# install/update ohmyposh
+curl -s https://ohmyposh.dev/install.sh | bash -s
 
