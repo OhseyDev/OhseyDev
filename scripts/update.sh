4881b1db4dbbe4fa0ef022a4236f2dc49e05e9ac
Ohsey <admin@proton.me>
2024-12-20 01:17:05 +0000

    Update config

--- scripts/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# update arch repository packages
-sudo pacman -Suy
-# update all installed aur packages
-paru -Sua
-# install/update ohmyposh
-curl -s https://ohmyposh.dev/install.sh | sudo bash -s
-
